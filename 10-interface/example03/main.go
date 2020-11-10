763174296ca4206ced7ba36a3d3825b4f3ad2c6e
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-11-11 02:08:48 +0800

    chore(10): fix golint

--- 10-interface/example03/main.go
+++ 10-interface/example03/main.go
@@ -4,31 +4,31 @@ import (
 	"fmt"
 )
 
-type IPV4 []byte
+type ipv4 []byte
 
 // Stringer is implemented by any value that has a String method,
 // which defines the ``native'' format for that value.
 // The String method is used to print values passed as an operand
 // to any format that accepts a string or to an unformatted printer
 // such as Print.
-func (s IPV4) String() string {
+func (s ipv4) String() string {
 	return fmt.Sprintf("%v.%v.%v.%v", s[0], s[1], s[2], s[3])
 }
 
 // GoString method, which defines the Go syntax for that value.
 // The GoString method is used to print values passed as an operand
 // to a %#v format.
-func (s IPV4) GoString() string {
+func (s ipv4) GoString() string {
 	return fmt.Sprintf("%v.%v.%v.%v", s[0], s[1], s[2], s[3])
 }
 
 func main() {
-	ipv4 := map[string]IPV4{
+	ips := map[string]ipv4{
 		"localhost": {127, 0, 0, 1},
 		"Google":    {8, 8, 8, 8},
 	}
 
-	for i, v := range ipv4 {
+	for i, v := range ips {
 		fmt.Printf("name: %s, ip: %v\n", i, v)
 		fmt.Printf("debug: %#v\n", v)
 	}
