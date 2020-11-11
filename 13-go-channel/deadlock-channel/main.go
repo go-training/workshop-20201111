98b549a36546e983754cb89479aab9c9592098d2
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-11-11 13:15:02 +0800

    chore(13): update dead lock

--- 13-go-channel/deadlock-channel/main.go
+++ 13-go-channel/deadlock-channel/main.go
@@ -14,12 +14,9 @@ func main() {
 		wg.Done()
 	}()
 	for j := 0; j < 5; j++ {
-		wg.Add(2)
+		wg.Add(1)
 		go func() {
-			i := 100
-			// copy the i value to channel
-			ch <- i
-			i = 101
+			ch <- 100
 			wg.Done()
 		}()
 	}
