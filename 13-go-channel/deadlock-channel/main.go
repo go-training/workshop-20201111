a99f4e82d1ae852e350f3844858769435dd3eaed
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-11-11 13:16:57 +0800

    chore(13): restricting data flow

--- 13-go-channel/deadlock-channel/main.go
+++ 13-go-channel/deadlock-channel/main.go
@@ -8,17 +8,17 @@ import (
 func main() {
 	wg := sync.WaitGroup{}
 	ch := make(chan int)
-	go func() {
+	go func(ch <-chan int) {
 		i := <-ch
 		fmt.Println(i)
 		wg.Done()
-	}()
+	}(ch)
 	for j := 0; j < 5; j++ {
 		wg.Add(1)
-		go func() {
+		go func(ch chan<- int) {
 			ch <- 100
 			wg.Done()
-		}()
+		}(ch)
 	}
 
 	wg.Wait()
